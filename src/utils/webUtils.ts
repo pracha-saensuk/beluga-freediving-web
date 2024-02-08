0d0ba7e03a64f521c897d10c138037e25f66c03c
Pracha Saensuk <pracha@quant.co.th>
2024-02-08 21:44:41 +0700

    Fix cookie bug cont.

--- src/utils/webUtils.ts
+++ src/utils/webUtils.ts
@@ -44,11 +44,10 @@ export function storeIfExists(key, value) {
     if(!cookieDomain){
       cookieDomain = generateDomainForCookie();
     }
-    // console.log({cookieDomain});
     if (value !== '' && !!value) {
       const cookies = parseJSON(Cookies.get(defaultKey)) as TrackingObject;
       const ls = parseJSON(localStorage.getItem(defaultKey)) as TrackingObject;
-      Cookies.set(defaultKey, JSON.stringify({...cookies, [key]: value}), {expires:365,});
+      Cookies.set(defaultKey, JSON.stringify({...cookies, [key]: value}), {expires:365,domain: cookieDomain});
       localStorage.setItem(defaultKey, JSON.stringify({...ls, [key]: value}));
     }
 }
