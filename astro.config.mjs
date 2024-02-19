75ba2126ab1f99e48afca0ed2f153f092f1b2232
Pracha Saensuk <pracha@quant.co.th>
2024-02-19 17:55:42 +0700

    Remove all analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,23 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
+// import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import tasks from './src/utils/tasks';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
 
-import { ANALYTICS, SITE, } from './src/utils/config.ts';
+import { /*ANALYTICS,*/ SITE, } from './src/utils/config.ts';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const whenExternalScripts = (items = []) =>
-  ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
-    ? Array.isArray(items)
-      ? items.map((item) => item())
-      : [items()]
-    : [];
+// const whenExternalScripts = (items = []) =>
+//   ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
+//     ? Array.isArray(items)
+//       ? items.map((item) => item())
+//       : [items()]
+//     : [];
 
 export default defineConfig({
   site: SITE.site,
@@ -56,39 +56,39 @@ export default defineConfig({
       },
     }),
 
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: [
-          'dataLayer.push',
-          'fbq',
-          // 'ttq.track',
-          // 'ttq.page',
-          // 'ttq.load'
-        ],
-         resolveUrl: function(url) {
-          //console.log("ResolveUrl: ", url)
-          if (url.hostname === "connect.facebook.net") {
-            const proxyUrl = url;
-            proxyUrl.host = 'fb-reverse-proxy.pracha-saensuk.workers.dev';
-            // console.log(proxyUrl.href);
-            return proxyUrl;
-          }
-          // if (url.hostname === "analytics.tiktok.com") {
-          //   const proxyUrl = url;
-          //   proxyUrl.host = 'tt-reverse-proxy.pracha-saensuk.workers.dev';
-          //   // console.log(proxyUrl.href);
-          //   return proxyUrl;
-          // }
-          // if (url.hostname === "www.googletagmanager.com") {
-          //   const proxyUrl = url;
-          //   proxyUrl.host = 'gtm-reverse-proxy.pracha-saensuk.workers.dev';
-          //   // console.log(proxyUrl.href);
-          //   return proxyUrl;
-          // }
-          return url;
-        },},
-      })
-    ),
+    // ...whenExternalScripts(() =>
+    //   partytown({
+    //     config: { forward: [
+    //       'dataLayer.push',
+    //       'fbq',
+    //       // 'ttq.track',
+    //       // 'ttq.page',
+    //       // 'ttq.load'
+    //     ],
+    //      resolveUrl: function(url) {
+    //       //console.log("ResolveUrl: ", url)
+    //       if (url.hostname === "connect.facebook.net") {
+    //         const proxyUrl = url;
+    //         proxyUrl.host = 'fb-reverse-proxy.pracha-saensuk.workers.dev';
+    //         // console.log(proxyUrl.href);
+    //         return proxyUrl;
+    //       }
+    //       // if (url.hostname === "analytics.tiktok.com") {
+    //       //   const proxyUrl = url;
+    //       //   proxyUrl.host = 'tt-reverse-proxy.pracha-saensuk.workers.dev';
+    //       //   // console.log(proxyUrl.href);
+    //       //   return proxyUrl;
+    //       // }
+    //       // if (url.hostname === "www.googletagmanager.com") {
+    //       //   const proxyUrl = url;
+    //       //   proxyUrl.host = 'gtm-reverse-proxy.pracha-saensuk.workers.dev';
+    //       //   // console.log(proxyUrl.href);
+    //       //   return proxyUrl;
+    //       // }
+    //       return url;
+    //     },},
+    //   })
+    // ),
 
     compress({
       CSS: true,
